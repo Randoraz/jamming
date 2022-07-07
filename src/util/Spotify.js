2198a7f7bd05f053bc8944820bd9fb6227466941
Randoraz <97787227+Randoraz@users.noreplay.github.com>
2022-07-07 11:06:07 +0200

    Change redirect URI to Netlify app link

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 const clientID = '5b6f8d1b85a543459fcfe35b9fa324cc';
+const redirectURI = 'https://randoraz_jammming.netlify.app';
 //const redirectURI = 'http://randoraz_jammming.surge.sh';
-const redirectURI = 'http://localhost:3000/';
 
 let token;
 const Spotify = {
